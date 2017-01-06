bdfe75415a624e197768753c3e100edc42a019d3
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-01-05 23:59:25 -0500

    add framework for weather api and output

--- script.js
+++ script.js
@@ -18,4 +18,16 @@ function geoLocation() {
         output.innerHTML = "<p>Here is the weather for " + city + ", " + state;
     });
 
+    $.getJSON('https://api.openweathermap.org/data/2.5/weather?zip=' + postal + ',' + country + "&appid=ec96c6ed7e722bdd15cfebffbff509a6", function(conditions) {
+
+        var main = response.main.split(',');
+        var conditons = {
+            temp: main[0]
+        };
+
+        console.log(temp);
+        var weatherConditions = document.getElementById("conditions");
+
+        conditions.innerHTML = "<p>The current temperature is " + temp;
+    });
 }
