e80d1ffb7f6f0f0a49b4d0cec18b339e0bf7575c
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-01-06 09:59:58 -0500

    Change location api to ip-api.com, rename conditions function to data

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 function geoLocation() {
     var output = document.getElementById("out");
 
-    $.getJSON('https://ipinfo.io/geo', function(response) {
+    /*$.getJSON('https://ipinfo.io/geo', function(response) {
         var loc = response.loc.split(',');
         var coords = {
             latitude: loc[0],
@@ -15,19 +15,32 @@ function geoLocation() {
         var postal = response.postal;
         var country = response.country;
 
+        output.innerHTML = "<p>Here is the weather for " + city + ", " + state;*/
+
+        $.getJSON('http://ip-api.com/json', function(response) {
+
+        var city = response.city;
+        var state = response.region;
+        var zip = response.zip;
+        var country = response.countryCode;
+        var ip = response.query;
+
         output.innerHTML = "<p>Here is the weather for " + city + ", " + state;
+
+
+        $.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + ',' + country + "&appid=ec96c6ed7e722bdd15cfebffbff509a6", function(data) {
+
+            var main = data.main.split(',');
+            var conditons = {
+                temp: main[0]
+            };
+
+            console.log(temp);
+            var weatherConditions = document.getElementById("conditions");
+
+            conditions.innerHTML = "<p>The current temperature is " + temp;
+        });
     });
 
-    $.getJSON('https://api.openweathermap.org/data/2.5/weather?zip=' + postal + ',' + country + "&appid=ec96c6ed7e722bdd15cfebffbff509a6", function(conditions) {
 
-        var main = response.main.split(',');
-        var conditons = {
-            temp: main[0]
-        };
-
-        console.log(temp);
-        var weatherConditions = document.getElementById("conditions");
-
-        conditions.innerHTML = "<p>The current temperature is " + temp;
-    });
 }
