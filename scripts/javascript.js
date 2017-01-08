2d6915536ce73dcf098ac1a84a704bd511b63d5b
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-01-07 21:56:45 -0500

    fixed to proper indentions

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,67 +1,66 @@
-
 //Geolocation Function is listed below.
 
 function geoLocation() {
     var output = document.getElementById("out");
 
-    if (!navigator.geolocation){
-    output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-    return;
-  }
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
 
-  function success(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
+    function success(position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
 
-    //output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+        //output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
-  /*  var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+        /*  var img = new Image();
+          img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
-    output.appendChild(img);*/
+          output.appendChild(img);*/
 
-    $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU', function (city) {
+        $.getJSON('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + latitude + ',' + longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU', function(city) {
 
-      var address = city.results[2].formatted_address;
-
-
-      var userAddressObj = document.getElementById(userAddress);
-
-      userAddress.innerHTML = "<p>Current condtions for " + address;
-
-    });
+            var address = city.results[2].formatted_address;
 
 
-$.ajax({
-  url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + latitude + ',' + longitude + "?units=us",
-  dataType: "jsonp",
-  success: function(data) {
+            var userAddressObj = document.getElementById(userAddress);
+
+            userAddress.innerHTML = "<p>Current condtions for " + address;
+
+        });
 
 
-    var temp = data.currently.temperature;
-    var description = data.currently.summary;
-    var hourlyDescription = data.hourly.summary;
+        $.ajax({
+            url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + latitude + ',' + longitude + "?units=us",
+            dataType: "jsonp",
+            success: function(data) {
 
-    console.log(data);
 
-    var current = document.getElementById(conditions);
+                var temp = data.currently.temperature;
+                var description = data.currently.summary;
+                var hourlyDescription = data.hourly.summary;
 
-    conditions.innerHTML = "<p>It is currently " + Math.round(temp) + " °F" + " and " + description;
+                console.log(data);
 
-    var hourly = document.getElementById(twentyfour);
+                var current = document.getElementById(conditions);
 
-    twentyfour.innerHTML = "<p> Expect " + hourlyDescription;
-  }
-});
+                conditions.innerHTML = "<p>It is currently " + Math.round(temp) + " °F" + " and " + description;
 
-}
+                var hourly = document.getElementById(twentyfour);
 
-  function error() {
-    output.innerHTML = "Unable to retrieve your location";
-  }
+                twentyfour.innerHTML = "<p> Expect " + hourlyDescription;
+            }
+        });
 
-  userAddress.innerHTML = "<p>Locating…</p>";
+    }
 
-  navigator.geolocation.getCurrentPosition(success, error);
+    function error() {
+        output.innerHTML = "Unable to retrieve your location";
+    }
+
+    userAddress.innerHTML = "<p>Locating…</p>";
+
+    navigator.geolocation.getCurrentPosition(success, error);
 
 }
