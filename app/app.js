632c7ae7af2f8befbe3819e8fb8aecbced28f4d4
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-04 20:24:51 -0500

    setup main app config template

---
+++ app/app.js
@@ -0,0 +1,14 @@
+(function() {
+    var app = angular.module('localWeather', ['ngRoute']);
+
+    app.config(function($routeProvider) {
+
+        $routeProvider
+            .when("/main", {
+                templateUrl: "app/views/main.html",
+                controller: "WxController"
+            });
+    });
+
+
+}());
