7749834913a2b4bf809ab6dd3147a6d5f55f4f73
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-06 20:34:37 -0500

    Add DarkSky API and add object to scope

--- app/app.js
+++ app/app.js
@@ -1,40 +1,37 @@
-(function() {
-    var app = angular.module('localWeather', []);
+var app = angular.module('localWeather', []);
 
 
-    var MainController = function($scope, $http) {
+var MainController = function($scope, $http) {
 
-        var onAddressComplete = function(address) {
-            $scope.userAddress = address.data;
+    var onAddressComplete = function(address) {
+        $scope.userAddress = address.data;
 
-        };
-
-        function success(pos) {
-            var crd = pos.coords;
-            $scope.coords = crd;
-
-
-            $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + crd.latitude + ',' + crd.longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU')
-                .then(onAddressComplete);
-
-
-            $http({
-                method: "JSONP",
-                dataType: "json",
-                url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + crd.latitude + ',' + crd.longitude + "?units=us"
-            }).
-            success(function(data) {
-                $scope.weather = data;
-            }).
-            error(function(data) {
-                $scope.weather = "Weather data could not be retrieved";
-            });
-        }
-
-
-        navigator.geolocation.getCurrentPosition(success);
     };
 
+    function success(pos) {
+        var crd = pos.coords;
+        $scope.coords = crd;
 
-    app.controller('MainController', MainController);
-}());
+
+
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + crd.latitude + ',' + crd.longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU')
+            .then(onAddressComplete);
+
+
+        $.ajax({
+            url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + crd.latitude + ',' + crd.longitude + "?units=us",
+            dataType: "jsonp",
+            success: function(data) {
+                $scope.weather = data;
+                console.log($scope.weather);
+            }
+        });
+
+    }
+
+
+    navigator.geolocation.getCurrentPosition(success);
+};
+
+
+app.controller('MainController', MainController);
