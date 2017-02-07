fbfe3f35264106ca663da8f86590875e5ffac456
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-06 20:36:45 -0500

    cleanup whitespace

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 var app = angular.module('localWeather', []);
 
-
 var MainController = function($scope, $http) {
 
     var onAddressComplete = function(address) {
@@ -12,11 +11,8 @@ var MainController = function($scope, $http) {
         var crd = pos.coords;
         $scope.coords = crd;
 
-
-
         $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + crd.latitude + ',' + crd.longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU')
             .then(onAddressComplete);
-
 
         $.ajax({
             url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + crd.latitude + ',' + crd.longitude + "?units=us",
@@ -28,10 +24,7 @@ var MainController = function($scope, $http) {
         });
 
     }
-
-
     navigator.geolocation.getCurrentPosition(success);
 };
-
 
 app.controller('MainController', MainController);
