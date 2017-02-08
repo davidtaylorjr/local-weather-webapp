878021ee9694184b5f0091f39978aa5398b3ea9b
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-07 20:58:31 -0500

    Add framework for weather service [#139321225]

---
+++ app/services/weather.js
@@ -0,0 +1,34 @@
+var weather = function($http) {
+
+    var onAddressComplete = function(address) {
+        $scope.userAddress = address.data;
+
+    };
+
+    function success(pos) {
+        var crd = pos.coords;
+        $scope.coords = crd;
+
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + crd.latitude + ',' + crd.longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU')
+            .then(onAddressComplete);
+
+
+        $.ajax({
+            url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + crd.latitude + ',' + crd.longitude + "?units=us",
+            dataType: "jsonp",
+            success: function(data) {
+                $scope.currentWeather = data.currently;
+                console.log($scope.currentWeather);
+                $scope.$apply();
+
+            }
+        });
+
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
+
+};
+
+var module = angular.module("localWeather");
+module.factory("weather", weather);
