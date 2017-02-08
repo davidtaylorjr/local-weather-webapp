4c6e796cb3cdfe30202ef8f9b3ab770f30031372
David Taylor Jr <davidtaylorjr1985@gmail.com>
2017-02-07 20:10:14 -0500

    move MainController to MainController.js and initiate ngRoute

--- app/controllers/MainController.js
+++ app/controllers/MainController.js
@@ -1,12 +1,35 @@
-(function() {
+var module = angular.module("localWeather");
 
-    var app = angular.module('localWeather');
+var MainController = function($scope, $http) {
 
-    var MainController = function($scope, $http, $location) {
-
-
-
+    var onAddressComplete = function(address) {
+        $scope.userAddress = address.data;
 
     };
 
-}());
+    function success(pos) {
+        var crd = pos.coords;
+        $scope.coords = crd;
+
+        $http.get('https://maps.googleapis.com/maps/api/geocode/json?latlng=' + crd.latitude + ',' + crd.longitude + '&key=AIzaSyAZch_gAq-6Ja3fUQ8sXStIhyB_dJ0mSgU')
+            .then(onAddressComplete);
+
+
+        $.ajax({
+            url: 'https://api.darksky.net/forecast/61f104c5d563f5c8aa29eca3beea2bde/' + crd.latitude + ',' + crd.longitude + "?units=us",
+            dataType: "jsonp",
+            success: function(data) {
+                $scope.currentWeather = data.currently;
+                console.log($scope.currentWeather);
+                $scope.$apply();
+
+            }
+        });
+
+    }
+
+    navigator.geolocation.getCurrentPosition(success);
+};
+
+
+app.controller('MainController', MainController);
