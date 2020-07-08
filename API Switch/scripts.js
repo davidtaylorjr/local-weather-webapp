e10c1b0366df28bc735f67c8fe3a29d31a6ac64d
David Taylor Jr <davidtaylorjr1985@gmail.com>
2020-07-08 11:29:27 -0500

    commented out test console logs for location

--- API Switch/scripts.js
+++ API Switch/scripts.js
@@ -5,12 +5,12 @@
     var lat = pos.coords.latitude;
     var lon = pos.coords.longitude;
   
-    console.log('Your current position is:');
+    /*console.log('Your current position is:');
     console.log(`Latitude : ${lat}`);
     console.log(`Longitude: ${lon}`);
     console.log(`More or less ${crd.accuracy} meters.`);
     console.log(`Latitude: ${lat}`);
-    console.log(crd);
+    console.log(crd);*/
 
     $.ajax({
         url: 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=ec96c6ed7e722bdd15cfebffbff509a6',
